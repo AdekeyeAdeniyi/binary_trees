3318aff190f91c7941a8d9ee2d9e3617949ac7f7
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-08-27 00:50:55 +0100

    traversing BST (postorder)

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,15 +5,16 @@
  * @func: function to use
  * Return: Nothing
  */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)){
-    if (tree == NULL || func == NULL)
-    {
-        return;
-    }
-    else
-    {
-        binary_tree_inorder(tree->left, func);
-		binary_tree_inorder(tree->right, func);
-    }
-		func(tree->n);
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+	}
+	func(tree->n);
 }
