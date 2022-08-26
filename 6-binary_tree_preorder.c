ae8b5c147717d1a5b8437cb9e5a8e9ff875f9d6c
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-08-27 00:30:17 +0100

    traversing BST (preorder)

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,14 +5,23 @@
  * @func: function to use
  * Return: Nothing
  */
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - print elements of tree using pre-order traversal
+ * @tree: tree to go through
+ * @func: function to use
+ * Return: Nothing
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
-    else
-    {
-        func(tree->n);
-        binary_tree_preorder(tree->left, tree->n);
-        binary_tree_preorder(tree->right, tree->n);
-    }
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 }
