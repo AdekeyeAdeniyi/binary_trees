e374faf689780423542c625a51a026ccb270b9e7
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-08-27 00:00:12 +0100

    update code

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,25 +1,24 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_delete - delete a node
- * if it exists it move down one level and add the new node first
- * @parent: parent of the specified node
- * @value: value of the node
- * Return: NULL if it fails or the new node
+ * binary_tree_delete - free an entire tree using recursion it free the node
+ * when the node left and right are NULL
+ * @tree: tree to free
+ * Return: Nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return;
-    }
-    else
-    {
-        if (tree != NULL){
-            binary_tree_delete(tree->right);
-            binary_tree_delete(tree->left);
-        }
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
+	}
 
-        free(tree);
-    }
 }
